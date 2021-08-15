e9ac91e2d0bd2cefb8f1563d353032e0bdf52530
Ronaldo do Carmo <ronaldokarmo@gmail.com>
2021-08-15 12:04:57 -0300

    Complementando validações de assert no body dos atributos (tags e category)

--- src/test/java/petstore/PetTests.java
+++ src/test/java/petstore/PetTests.java
@@ -7,9 +7,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
 
-public class Pet {
-    String uri = "https://petstore.swagger.io/v2/pet";
+public class PetTests {
+    String uri = "https://petstore.swagger.io/v2";
 
     public String getJson(String json) throws IOException {
         return new String(Files.readAllBytes(Paths.get(json)));
@@ -21,12 +23,20 @@ public class Pet {
 
         given()
                 .contentType("application/json")
-                .log().all()
                 .body(bodyJson)
         .when()
-                .post(uri)
+                .post(uri.concat("/pet"))
         .then()
                 .log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("category.name", is("Dogs"))
+                .body("name", is("Nainai"))
+                .body("tags.name", contains("adopted"))
+                .body("status", is("available"));
+    }
+
+    @Test
+    public void consultarPet() {
+
     }
 }
