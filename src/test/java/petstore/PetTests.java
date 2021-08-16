34e0e22bf72d27b3e5b07702ce555c87aaae8ed8
Ronaldo do Carmo <ronaldokarmo@gmail.com>
2021-08-16 09:26:39 -0300

    Adicionado novo teste para DeletarPet

--- src/test/java/petstore/PetTests.java
+++ src/test/java/petstore/PetTests.java
@@ -11,25 +11,29 @@ import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 
 public class PetTests {
-    private String uri = "https://petstore.swagger.io/v2";
-    private String petId;
+    private final String uri = "https://petstore.swagger.io/v2";
+    String petId;
 
     public String getJson(String json) throws IOException {
         return new String(Files.readAllBytes(Paths.get(json)));
+    }
+
+    public static String removerCarecteres(String str) throws  NullPointerException {
+        return str.replaceAll("[-+.^:,<>]","");
     }
 
     @Test(priority = 1)
     public void incluirPet() throws IOException {
         String bodyJson = getJson("db/pet1.json");
 
-        petId = given()
-                //.log().all()
+        String petId = given()
+                .log().all()
                 .contentType("application/json")
                 .body(bodyJson)
         .when()
                 .post(uri.concat("/pet"))
         .then()
-                //.log().all()
+                .log().all()
                 .statusCode(200)
                 .body("category.name", is("Dogs"))
                 .body("name", is("Nainai"))
@@ -38,40 +42,55 @@ public class PetTests {
         .extract()
                 .jsonPath().getString("id");
 
-        System.out.println("petId:".concat(petId));
+        System.out.println("petId: ".concat(petId.toString()));
     }
 
     @Test(priority = 2)
     public void consultarPet() {
-        String petId = "1984020712";
+        //String petId = "1984020712";
 
         given()
-                //.log().all()
+                .log().all()
                 .contentType("application/json")
-        .when()
+                .when()
                 .get(uri.concat("/pet/").concat(petId))
-        .then()
-                //.log().all()
+                .then()
+                .log().all()
                 .statusCode(200)
-                //.body("id", is(petId.toString()))
+                .body("id", is(removerCarecteres(petId)))
                 .body("name", is("Nainai"))
                 .body("status", is("available"));
     }
 
     @Test(priority = 3)
-    public void AlterarPet() throws IOException {
+    public void alterarPet() throws IOException {
         String bodyJson = getJson("db/pet2.json");
 
         given()
-                //.log().all()
+                .log().all()
                 .contentType("application/json")
                 .body(bodyJson)
         .when()
                 .put(uri.concat("/pet"))
         .then()
-                //.log().all()
+                .log().all()
                 .statusCode(200)
                 .body("name", is("Jazz"))
                 .body("status", is("adopted"));
+    }
+
+    @Test(priority = 4)
+    public void  excluirPet(){
+        given()
+                .log().all()
+                .contentType("application/json")
+        .when()
+                .delete(uri.concat("/pet/").concat(petId))
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is ("unknown"))
+                .body("message", is(petId));
     }
 }
